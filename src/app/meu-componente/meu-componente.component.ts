1da2b89986bc8c9564a73bd1c172bb29c49e96c1
Flavio <flavio.santos@justdigital.com.br>
2019-02-20 12:39:03 -0300

    First component

---
+++ src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-componente-component',
+    templateUrl: './meu.component.component.html'
+})
+
+export class MeuComponenteComponent {
+    Titulo = ' Diante e além';
+}
